ce5a407a9c6f22f4c595bef2c166be4a36b9a59f
Sekhar Paladugu <sekharp@gmail.com>
2016-04-10 18:27:12 -0500

    iteration 9 done

--- Bingo.elm
+++ Bingo.elm
@@ -5,6 +5,13 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import String exposing (toUpper, repeat, trimRight)
 
+newEntry phrase points id =
+  { phrase = phrase,
+  points = points,
+  wasSpoken = False,
+  id = id
+  }
+
 title message times =
   message ++ " "
     |> toUpper
@@ -21,16 +28,16 @@ pageFooter =
     [ a [ href "https://pragmaticstudio.com" ]
         [ text "The Pragmatic Studio" ] ]
 
-entryItem phrase points =
+entryItem entry =
   li [ ]
-  [ span [ class "phrase" ] [ text phrase ],
-    span [ class "points" ] [ text (toString points) ]
+  [ span [ class "phrase" ] [ text entry.phrase ],
+    span [ class "points" ] [ text (toString entry.points) ]
   ]
 
 entryList =
   ul [ ]
-    [ entryItem "Future-Proof" 100,
-      entryItem "Doing Agile" 200
+    [ entryItem (newEntry "Future-Proof" 100 1),
+      entryItem (newEntry "Doing Agile" 200 2)
     ]
 
 view =
