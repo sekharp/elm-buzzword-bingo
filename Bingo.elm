371a573e1745b3d615417e55b49e855ed4c05d77
Sekhar Paladugu <sekharp@gmail.com>
2016-04-10 19:45:58 -0500

    iteration 14 done

--- Bingo.elm
+++ Bingo.elm
@@ -66,22 +66,25 @@ pageFooter =
     [ a [ href "https://pragmaticstudio.com" ]
         [ text "The Pragmatic Studio" ] ]
 
-entryItem entry =
+entryItem address entry =
   li [ ]
   [ span [ class "phrase" ] [ text entry.phrase ],
     span [ class "points" ] [ text (toString entry.points) ],
     button
-      [ class "delete", onClick address Delete entry.id ]
-      [ text "-" ]
+      [ class "delete", onClick address (Delete entry.id) ]
+      [ ]
   ]
 
-entryList entries =
-  ul [ ] (List.map entryItem entries)
+entryList address entries =
+  let
+    entryItems = List.map (entryItem address) entries
+  in
+    ul [ ] entryItems
 
 view address model =
   div [ id "container" ]
   [ pageHeader,
-    entryList model.entries,
+    entryList address model.entries,
     button
       [ class "sort", onClick address Sort ]
       [ text "Sort" ],
